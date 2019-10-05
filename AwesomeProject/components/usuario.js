aee6910d16072937f33fa9969e48df59a7f060ba
Estanipp <estanipp@gmail.com>
2019-10-05 20:59:50 -0300

    Clase usuario basica

---
+++ AwesomeProject/components/usuario.js
@@ -0,0 +1,10 @@
+export default class usuario{
+    constructor(usuario,nombre,apellido,direccion,telefono,email){
+        this.usuario = usuario;
+        this.apellido = apellido
+        this.direccion = direccion;
+        this.email = email;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+}
