6f29eeb12328c3adc9722d9fc60571bdb2c5c094
Guido <guidomodarelli25@gmail.com>
2019-10-14 18:46:43 -0300

    Actualice server.js

--- Base de datos/server.js
+++ Base de datos/server.js
@@ -1,36 +1,35 @@
-var mysql = require('mysql');
+var mysql = require('../AwesomeProject/node_modules/mysql');
 var resultado;
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "lautaro",
-  password: "Lautaro3.0",
-insecureAuth : true,
-  database: "talleragiles"
+    host: "localhost",
+    user: "usuario",
+    password: "contraseña",
+    insecureAuth : true,
+    database: "talleragiles"
 });
 
 con.connect(function(err) {
-  if (err) throw err;
+    if (err) throw err;
 });
 
-
-
-con.query("SELECT * FROM Cuenta", function (err, result, fields) {
+con.query("SELECT * FROM Cuenta", function (err, result) {
     if (err) throw err;
     resultado = result;
-console.log(result[0].username);
-  });
+    if (resultado.length != 0) {
+        console.log(resultado[0].username);
+    }
+});
 
 const http = require('http');
-
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(resultado[0].username);
+const server = http.createServer((res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(resultado[0].username);
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
