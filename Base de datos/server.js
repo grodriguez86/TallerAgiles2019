404d878747c2c07a195d0e9bdc3ae1a4b4d92ffe
lautaroialea <lautaro.exa@gmail.com>
2019-10-21 14:46:45 -0300

    bugfixes

--- Base de datos/server.js
+++ Base de datos/server.js
@@ -7,11 +7,11 @@ var INSERT_INTO_USER = "INSERT INTO Cuenta (username, password, admin, mail, fec
 var INSERT_INTO_PERMISOS = "insert into Permisos (Cuenta_username, Tag_idTag, escritura, lectura) VALUES (?, ?, ?, ?)";
 var INSERT_INTO_COMENTARIO = "INSERT INTO Comentario (Cuenta_username, Noticia_Contenido_idContenido, Texto_Comentario) VALUES (?, ?, ?)";
 var INSERT_INTO_TAG = "INSERT INTO Tag (idTag, nombre) VALUES (?, ?)";
-var DELETE_CUENTA_USERNAME = "DELETE FROM Cuenta WHERE username = ?";
+var DELETE_CUENTA_USERNAME = "DELETE FROM Cuenta WHERE username=?";
 var GET_CUENTA = "SELECT * FROM Cuenta";
 var GET_CUENTA_USERNAME = "SELECT * FROM Cuenta where username=?";
-var GET_TAGS = "SELECT nombre FROM Tag";
-var GET_PERMISOS_USERNAME = "SELECT escritura, lectura FROM Permisos WHERE username = ?";
+var GET_TAGS = "SELECT * FROM Tag";
+var GET_PERMISOS_USERNAME = "SELECT Tag_idTag, escritura, lectura FROM Permisos WHERE Cuenta_username=?";
 var GET_NOTICIAS = "SELECT c.titulo, c.descripcion, n.contenido FROM Contenido c INNER JOIN Noticia n ON (c.idContenido = n.Contenido_idContenido)";
 var GET_NOTICIA_ID = "SELECT c.titulo, c.descripcion, n.contenido FROM Contenido c INNER JOIN Noticia n ON (c.idContenido = n.Contenido_idContenido) WHERE n.Contenido_idContenido=?";
 
@@ -55,7 +55,7 @@ app.get('/user/:username', function (req, res) {
 
     con.query(GET_CUENTA_USERNAME, username, function (error, results, fields) {
         if (error) throw error;
-        return res.send({ error: false, data: results, message: 'users list.' });
+        return res.send({ error: false, data: results, message: 'user listed.' });
     });
 });
 
@@ -63,7 +63,7 @@ app.get('/tags', function(req, res) {
     con.query(GET_TAGS, function (err, result) {
         if (err) throw err;
         if (result.length != 0) {
-            return res.send({ error: false, data: result, message: 'users list.' });
+            return res.send({ error: false, data: result, message: 'tags list.' });
         }    
     });
 })
@@ -75,19 +75,19 @@ app.get('/user/:username/permisos', function(req, res) {
     if (!username) {
         return res.status(400).send({ error: true, message: 'Please provide username' });
     }
-
-    con.query(GET_PERMISOS_USERNAME, function(req, res) {
+    console.log(username);
+    con.query(GET_PERMISOS_USERNAME, username, function(err, result) {
         if (err) throw err;
         if (result.length != 0) {
-            return res.send({ error: false, data: result, message: 'users list.' });
+            return res.send({ error: false, data: result, message: 'permissions list.' });
         }    
     });
 })
 
 app.get('/noticias', function(req, res) {
     con.query(GET_NOTICIAS, function(error, results, fields) {
-        if (err) throw err;
-        return res.send({ error: false, data: results, message: 'users list.' });
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'news list.' });
     });
 })
 
@@ -99,9 +99,9 @@ app.get('/noticias/:id', function(req, res) {
         return res.status(400).send({ error: true, message: 'Please provide username' });
     }
 
-    con.query(GET_NOTICIA_ID, function(error, results, fields) {
-        if (err) throw err;
-        return res.send({ error: false, data: results, message: 'users list.' });
+    con.query(GET_NOTICIA_ID, id, function(error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'new listed.' });
     });
 })
 
@@ -111,7 +111,7 @@ app.post('/user', function(req, res) {
     var user = req.body;
 
     if (!user) {
-        return res.status(400).send({ error: true, message: 'Please provide username' });
+        return res.status(400).send({ error: true, message: 'Please provide correct parameters' });
     }
 
     con.query(INSERT_INTO_USER, [user.username, user.password, user.admin, user.mail, user.fecha_nacimiento, user.telefono, user.nombre, user.apellido, user.direccion], 
@@ -146,21 +146,21 @@ app.post('/label', function(req, res) {
         return res.status(400).send({ error: true, message: 'Error in parameters' });
     }
 
-    con.query(INSERT_INTO_TAG, function(error, results, fields) {
+    con.query(INSERT_INTO_TAG,[label.id,label.nombre], function(error, results, fields) {
         if (error) throw error;
-        return res.send({ error: false, data: results, message: 'New user has been created successfully.' });
+        return res.send({ error: false, data: results, message: 'New label has been created successfully.' });
     });
 })
 
 app.post('/user/:username/permisos', function(req, res) {
-    
+    var username = req.params.username;
     var permisos = req.body;
 
     if (!permisos) {
         return res.status(400).send({ error: true, message: 'Error in parameters' });
     }
 
-    con.query(INSERT_INTO_PERMISOS, username, function(error, results, fields) {
+    con.query(INSERT_INTO_PERMISOS, [username,permisos.tag,1,1], function(error, results, fields) {
         if (error) throw error;
         return res.send({ error: false, data: results, message: 'New user has been created successfully.' });
     });
@@ -174,7 +174,7 @@ app.delete('/user', function(req, res) {
         return res.status(400).send({ error: true, message: 'Please provide username' });
     }
 
-    con.query(DELETE_CUENTA_USERNAME, username, function(error, results, fields) {
+    con.query(DELETE_CUENTA_USERNAME, user.nombre, function(error, results, fields) {
         if (error) throw error;
         return res.send({ error: false, data: results, message: 'User has been deleted successfully.' });
     });
