a31dc7187dc91ac1d5f446a93498a7d0573aec92
Lucas Olivera <oliveralucasm@gmail.com>
2019-10-23 11:22:45 -0300

    Initial News View

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -6,9 +6,19 @@ import Noticia from './components/Noticia.js';
 export default class Ejemplo1 extends Component {
   
   render() {
+    n = new Noticia("1",
+                    "Lorem ipsum",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam congue elit ut purus porttitor ornare. Mauris vel pellentesque velit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean mi eros, vehicula in euismod id, faucibus nec velit. Aliquam erat volutpat. Cras ultricies, dolor in ornare congue, ex dolor maximus est, non rutrum tellus lectus ultrices lectus. Quisque rhoncus, nisi in dictum fringilla, urna metus blandit lorem, sed pharetra dolor massa porta tortor. Morbi rhoncus velit a pharetra tincidunt. Curabitur ultricies dolor a tellus condimentum, vel consectetur leo condimentum. Etiam volutpat mi at ante efficitur, non bibendum augue posuere.\
+\
+                    Fusce fermentum porttitor erat a ultrices. Nullam vitae sem rutrum, placerat elit quis, faucibus quam. Donec quis nisi non neque pellentesque lobortis vel ac odio. Nulla in nunc vehicula, sollicitudin ligula vitae, gravida felis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam lorem dolor, porttitor ac elit ullamcorper, cursus elementum enim. Nam non fringilla elit. Vestibulum elementum malesuada malesuada. Aliquam hendrerit dapibus nisi ac tincidunt. Integer mauris ligula, euismod vel vulputate id, rutrum at lectus. Proin ut rhoncus velit. Etiam vulputate mi ac turpis finibus, eleifend iaculis velit pellentesque. Nunc tempor diam vestibulum orci cursus ultricies in molestie nibh. Pellentesque sed pretium sem. Sed ex dui, ultrices non accumsan eu, tempor sit amet diam.\
+                    \
+                    Pellentesque leo lacus, dictum eget tempus at, malesuada eget tortor. Quisque tempor eu ante eu pretium. Morbi justo lorem, ornare ut erat sit amet, pretium sodales purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec non nunc et ante blandit commodo. Phasellus a leo ut mauris lacinia scelerisque. Sed non accumsan massa. Cras placerat ac libero sit amet condimentum. Ut velit lacus, vulputate a pretium a, ultrices in enim.",
+                     "crimen",
+                     "Autor",
+                     "23/10/2019")
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <NoticiaView noticia = {new Noticia("1","Primera noticia","asjdghasdhgasdgh","crimen","yo","hoy")} />
+      <View style={{ flex: 1}}>
+        <NoticiaView noticia = {n} />
       </View>
     );
   }
--- AwesomeProject/views/NoticiaView.js
+++ AwesomeProject/views/NoticiaView.js
@@ -1,92 +1,108 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, Button, Alert } from 'react-native';
 import Noticia from '../components/Noticia.js';
 
 export default class NoticiaView extends Component{
 
-    render(){
+  render(){  
+    return(
+      
+      <View style={styles.container}>
+        <ScrollView>
+          <View style={styles.headerTitulo}>
+
+              <Text style={styles.textDate}>{this.props.noticia.fecha}</Text>
+
+              <Text style={styles.textTitulo}>{this.props.noticia.titulo}</Text>         
+
+          </View>
+          <View style={styles.informacion}>
+
+            <Text style={styles.textStyleNoticia}>{this.props.noticia.cuerpo}</Text>
         
-        return(
-        <View style={styles.container}>
-          <View style={styles.titulo}>
-            <View style={{flex:1}}>
-                <View>
-                  <Text style={{fontSize:13, color:'white', textAlign:'right', fontWeight: 'bold'}}>{this.props.noticia.fecha}</Text>
-                </View>
-               <View>
-                 <Text style={{fontSize:26, color:'white', textAlign:'center', fontWeight: 'bold', paddingBottom:5}}>{this.props.noticia.titulo}</Text>
-               </View>             
-            </View>
+            <Text style={styles.textStyleAutor}>{this.props.noticia.autor}</Text>
+
           </View>
-            <View style={styles.informacion}>
-              <View style={styles.grupo}>
-                <Text style={styles.textStyle2}>{this.props.noticia.cuerpo}</Text>
-              </View>
-              <View style={styles.grupo}>
-                <Text style={styles.textStyle2}>{this.props.noticia.autor}</Text>
-              </View>
-            </View>
-          </View>
-        );
-      }
-    }
-    
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-      titulo: {
-        backgroundColor: '#072F46',
-        alignItems: 'flex-end',
-        justifyContent: 'flex-end',
-        flexDirection: 'row',
-        height: 100,
-      },
-      botones: {
-        backgroundColor: '#eeeeee',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'row',
-        height: 80,
-      },
-      informacion: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        backgroundColor: '#eeeeee',
-      },
-      grupo: {
-        height:100,
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'row',
-      },
-      columnaFlex1:{
-        flex:1,
-      },
-      columna: {
-        flex: 1,
-        height:100,
-        alignItems: 'center',
-      },
-      textStyle:{
-        flex:1,
-        flexDirection: 'row',
-        backgroundColor: 'white',
-        textAlign:'center',
-        marginHorizontal: 10,
-        borderWidth: 1,
-        borderRadius:5,
-      },
-      textStyle2:{
-        flex:1,
-        color :'#072F46',
-        textAlign:'left',
-        fontWeight: 'bold',
-        fontSize:20,
-        margin: 10,
-      }
-    });
+        </ScrollView>
+      </View>
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#eeeeee',
+    alignItems: 'stretch',
+  },
+  headerTitulo: {
+    flex: 1,
+    backgroundColor: '#072F46',
+    alignItems: 'stretch',
+  },
+  textTitulo: {
+    flex: 9,
+    fontSize:26, 
+    color:'white', 
+    textAlign:'center', 
+    fontWeight: 'bold', 
+    paddingBottom:5,
+  },
+  textDate: {
+    fontSize:13,
+    color:'white',
+    textAlign:'right',
+    fontWeight: 'bold',
+    paddingTop: 20,
+  },
+  botones: {
+    backgroundColor: '#eeeeee',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    height: 80,
+  },
+  informacion: {
+    flex: 7,
+    justifyContent: 'space-between',
+    backgroundColor: '#eeeeee',
+  },
+  grupo: {
+    height:100,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  columnaFlex1:{
+    flex:1,
+  },
+  columna: {
+    flex: 1,
+    height:100,
+    alignItems: 'center',
+  },
+  textStyle:{
+    flex:1,
+    //flexDirection: 'row',
+    backgroundColor: 'white',
+    textAlign:'center',
+    //marginHorizontal: 10,
+    //borderWidth: 1,
+    //borderRadius:5,
+  },
+  textStyleNoticia:{
+    flex: 9,
+    color:'#072F46',
+    textAlign:'justify',
+    fontSize:18,
+  },
+  textStyleAutor: {
+    flex: 1,
+    color:'#072F46',
+    textAlign:'right',
+    fontSize:16,
+    fontStyle: 'italic'
+  }
+});
